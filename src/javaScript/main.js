f2719c088fbdc443dce051c4bd1d67e1ee45d526
YoungPadawan <115190504+JovemPadawan@users.noreply.github.com>
2023-03-06 20:21:47 -0300

    alteracao diretorios 2

---
+++ src/javaScript/main.js
@@ -0,0 +1,9 @@
+document.getElementById("myForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const url = window.location.href;
+    const newLocation = url.slice(0, url.length - 11);
+
+    window.location.href = `${newLocation}/pageend.html`
+
+});
